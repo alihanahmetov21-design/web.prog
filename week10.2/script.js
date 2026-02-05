e2eaa9c2c1b4afc0394e12ea3bf1f4cbafa57480
Alikhan <alikhanakhmetov04@gmail.com>
2026-02-05 20:23:10 +0500

    Add initial HTML, CSS, and JavaScript files for event handling exercises

---
+++ week10.2/script.js
@@ -0,0 +1,54 @@
+let button = document.querySelector("#jsbutton");
+
+button.addEventListener("click", () => {
+    alert("Button clicked")
+});
+
+
+//Задание 3: Множественные события на одном элементе
+
+let div = document.querySelector("#myDiv");
+div.addEventListener("click", () => {
+    div.style.backgroundColor = "blue";
+});
+
+div.addEventListener("mouseover", () => {
+    console.log("Элемент нажат")
+});
+
+//
+//Задание 4: Работа с объектом события `event`
+
+let input = document.querySelector("#textinput");
+
+input.addEventListener("keydown", (event) => {
+    console.log(event.key)
+});
+
+
+//задание 5: Отмена действия по умолчанию//
+let link = document.querySelector("#myLink");
+
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("Переход по ссылке отменен")
+});
+
+
+//Задание 6: Делегирование событий
+
+let list = document.querySelector("#list");
+list.addEventListener("click",(event) => {
+    if(event.target && event.target.nodeName === 'LI'){
+        console.log("Элемент списка " + event.target.textContent + "был нажат")
+    }
+
+});
+
+
+//Задание 7: События клавиатуры
+let keyboardinput = document.querySelector("#keyboardInput");
+keyboardinput.addEventListener("keydown", (event) => {
+    console.log('event code')
+
+});
